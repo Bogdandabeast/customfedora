91610f18ad0e39214951078f68ce825f784b8875
Bogdandabeast <bogdan.console@gmail.com>
2025-12-29 19:35:17 +0100

    fix: moving into symlink dir

--- files/scripts/copysymlinkconfigs.sh
+++
@@ -1,9 +0,0 @@
- #!/usr/bin/env bash
- set -euo pipefail
-
- # Script para mover random-wallpaper.sh a su ubicación final y darle permisos de ejecución
-
- mkdir -p /usr/local/bin
- cp /usr/bin/random-wallpaper.sh /usr/local/bin/random-wallpaper.sh
- chmod +x /usr/local/bin/random-wallpaper.sh
- rm /usr/bin/random-wallpaper.sh # Eliminar la copia temporal
---
+++ files/scripts/permissions.sh
@@ -0,0 +1,6 @@
+ #!/usr/bin/env bash
+ set -euo pipefail
+
+ # Script para dar permisos de ejecución
+
+ chmod +x /usr/local/bin/random-wallpaper.sh
